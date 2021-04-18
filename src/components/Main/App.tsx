a3a9c8f943e45058fc1bbc6f7e3de96b560a8682
Matt <mrusso@ips-yes.com>
2021-04-18 19:37:52 -0400

    Add styles fro about section

--- src/components/Main/App.tsx
+++ src/components/Main/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import {Header} from '../Header/Header';
 import {About} from '../About/About';
 import {Sections} from './SectionTypes';
 import {useScroll} from '../../hooks/useScroll';
 
 function App() {
-    const { getRef, scrollToElement } = useScroll({});
+    const {getRef, scrollToElement} = useScroll({});
 
     return (
         <div className='App'>
             <Header onAboutClick={scrollToElement}/>
-            <About aboutRef={getRef(Sections.About)}/>
+            <div className={'main_content'}>
+                <About aboutRef={getRef(Sections.About)}/>
+            </div>
         </div>
     );
 }
