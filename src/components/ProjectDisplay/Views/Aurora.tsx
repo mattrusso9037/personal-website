5477031b872e54a3a7fb981eee923aa7a3c67806
Matt Russo <mattrusso@gmail.com>
2022-09-11 20:23:18 -0400

    Fixed errors

--- src/components/ProjectDisplay/Views/Aurora.tsx
+++ src/components/ProjectDisplay/Views/Aurora.tsx
@@ -9,28 +9,28 @@ export const Aurora: React.FC = () => {
                     <div>The Undo/Redo engine considers the state of each
                     machine vision tool, and allows users to undo and redo ROI movements, rotations, and settings configurations quickly. Since these ROI's must be pixel perfect, it's common for users to jump back and forth
                     through states to position them correctly.</div>
-                    <img src={'./assets/projects/pixel_count.png'} />
+                    <img alt='Pixel Count Tool' src={'./assets/projects/pixel_count.png'} />
                 </div>
                 <div className={'container'}>
                     <label>Golden Image Compare</label>
                     <div>Golden image compare utilizes synchronized HTML5 canvases to allow users to view images side by side. Users are able to pan
                     and zoom images in tandem. It allows users to compare their current subject image to a model image and inspect the differences, so they can adjust their camera settings appropriately.</div>
-                    <img src={'./assets/projects/golden_image_compare_2.png'} />
-                    <a target={'_blank'} href={'https://www.youtube.com/watch?v=n-d0GhVa43I'}>View Video</a>
+                    <img alt='Image Comparison' src={'./assets/projects/golden_image_compare_2.png'} />
+                    <a target={'_blank'} rel='noreferrer' href={'https://www.youtube.com/watch?v=n-d0GhVa43I'}>View Video</a>
                 </div>
                 <div className={'container'}>
                     <label>QuickDraw</label>
                     <div>To streamline the process of adding machine vision tools, a QuickDraw feature was built which enables users to define a
                         region of interest, and add a tool to the canvas area with that defined region. I implemented a canvas menu architecture, allowing for multiple menus to be defined for different uses. The menu is contextually aware, positioning
                     itself to ensure it remains within the bounds of the application.</div>
-                    <img src={'./assets/projects/quickdraw.png'} />
-                    <img src={'./assets/projects/quickdraw2.png'} />
+                    <img alt='QuickDraw' src={'./assets/projects/quickdraw.png'} />
+                    <img alt='QuickDraw' src={'./assets/projects/quickdraw2.png'} />
                 </div>
                 <div className={'container'}>
                     <label>Dynamic File Menu</label>
                     <div>The Aurora application had a need for a dynamic file menu based on user's location within the app. To satisfy this requirement, I built a
                     configuration-based file menu that dynamically changes as needed.</div>
-                    <img src={'./assets/projects/file_menu.png'} />
+                    <img alt='File Menu' src={'./assets/projects/file_menu.png'} />
                 </div>
                 <iframe
                     src={`https://www.youtube.com/embed/_6Eenn8dWDY`}
