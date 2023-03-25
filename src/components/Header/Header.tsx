c9ec43c30f9f979cce422aeabb2a3b4708d57d91
Matt Russo <mattrusso@gmail.com>
2023-03-25 07:04:20 -0400

    feat(header): hero img updates

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
-import React, {FC} from 'react';
+import React, { FC } from 'react';
 import './header.scss';
-import {ScrollTo} from '../../hooks/useScroll';
-import {Sections} from '../Main/SectionTypes';
+import { ScrollTo } from '../../hooks/useScroll';
+import { Sections } from '../Main/SectionTypes';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
@@ -11,24 +11,24 @@ interface IHeaderProps {
 
 export const Header: FC<IHeaderProps> =
     ({
-         onAboutClick,
-     }) => {
+        onAboutClick,
+    }) => {
 
-    function onClick(): void {
-        onAboutClick(Sections.About);
-    }
+        function onClick(): void {
+            onAboutClick(Sections.About);
+        }
 
         return (
-            <div className={'Header'}>
-                <img alt={'Earth from space'} src={'/assets/nasa_img.jpg'}/>
+            <header className={'Header'}>
+                <img alt={'A colorful image of a laptop surrounded by planets, AI generated.'} src={'/assets/hero_2.png'} />
                 <div>
                     <div>
-                        <h1>Hi I'm <span>Matt Russo</span>.<br/>I'm a Software Engineer.</h1>
+                        <h1>Hi I'm <span>Matt Russo</span>.<br />I'm a Software Engineer.</h1>
                     </div>
-                <div>
-                    <FontAwesomeIcon className={'fa-thin'} onClick={onClick} icon={faChevronDown} color={'white'} size={'3x'} />
+                    <div>
+                        <FontAwesomeIcon className={'fa-thin'} onClick={onClick} icon={faChevronDown} color={'white'} size={'3x'} />
+                    </div>
                 </div>
-                </div>
-            </div>
+            </header>
         )
     };
